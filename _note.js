746c99b320d7c1765863fa71e87f8f881dae9984
mdmmalamin <mdmmalamin@gmail.com>
2022-10-23 15:57:49 +0600

    file naming fixed

---
+++ _note.js
@@ -0,0 +1,37 @@
+/* 
+----------------------------------------
+    DevsDenBD Info Needed
+----------------------------------------
+1. DevsDenBD Logo max hight 200px (png and [if possible svg])
+& 64*64 ico formate only <D> icon or png formate... it's for website title
+
+2. DevsDenBD github link
+3. DevsDenBD Facebook page link
+4. DevsDenBD Twitter link
+5. DevsDenBD Linkdin link
+6. DevsDenBD Slack link
+7. DevsDenBD Whatsapp number
+8. DevsDenBD Contact number
+9. DevsDenBD Email
+10. DevsDenBD office address
+
+11. Top 3 banner card's Title & Description
+
+12. Services header section: Title & Description
+13. Services Name & Description
+
+14. Team Member Header Section: Title & Description
+15. DevsDenBD Team member's info: 
+    1) Name
+    2) Position
+    3) twitter link
+    4) Linkedin link
+    5) Github link
+    6) image
+
+And web site এর যে কোনো style, headline, title, color, hover  effect or anything change করা অথবা Add করা যাবে।
+
+
+*/
+
+/* content="Devs Den BD tech is a custom software development company of qa automation, software quality assurance, web designers, mobile app development, web development." */
