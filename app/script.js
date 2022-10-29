ff0fcb4b2273dd0e7997af52a4b9267812b0b5c4
mdmmalamin <mdmmalamin@gmail.com>
2022-10-30 00:09:03 +0600

    service's card hight fixed & team member info updated

---
+++ app/script.js
@@ -0,0 +1,65 @@
+const teamMember = [
+    {
+        fullName: 'Md Shahnewaz Zaman',
+        position: 'Tech Lead',
+        title: '',
+        github: 'https://github.com/msz5213',
+        linkedin: 'https://www.linkedin.com/in/zamanmd/',
+        twitter: '',
+        website: 'https://msz5213.github.io/mdzamanportfolio/'
+    },
+    {
+        fullName: 'Md. Mahbubur Rahman (TOM)',
+        position: 'SQA Engineer',
+        title: 'Specialized in Automation',
+        github: 'https://github.com/Mahbub091',
+        linkedin: 'https://www.linkedin.com/in/mahbubrahman091/',
+        twitter: 'https://twitter.com/tom_sqa',
+        website: ''
+    },
+    {
+        fullName: 'Talha Bin Ashraf',
+        position: 'SQA Engineer',
+        title: 'Specialized in Automation',
+        github: 'https://github.com/TalhaBinAshraf1',
+        linkedin: 'https://www.linkedin.com/in/talha-sqa-engineer/',
+        twitter: 'https://twitter.com/talhabinashraf',
+        website: ''
+    },
+    {
+        fullName: 'Iqbal',
+        position: 'SQA Engineer',
+        title: 'Specialized in Automation',
+        github: 'https://github.com/',
+        linkedin: 'https://www.linkedin.com/',
+        twitter: 'https://twitter.com/',
+        website: ''
+    },
+    {
+        fullName: 'Muhibullah',
+        position: 'Senior Software Developer',
+        title: 'Front-End',
+        github: 'https://github.com/muhib-dev',
+        linkedin: 'https://www.linkedin.com/in/muhibullah-khandaker/',
+        twitter: '',
+        website: 'https://docs.google.com/document/d/1MWCXVYZEl6aXu4zZst2fEW9qLHkViZyPUMhvJt99Xf0/'
+    },
+    {
+        fullName: 'Shahed Al Mahmud',
+        position: 'Senior Software Developer',
+        title: 'Back-End',
+        github: 'http://www.github.com/shahed-webdev',
+        linkedin: 'http://linkedin.com/in/shahed-al-mahmud',
+        twitter: '',
+        website: 'https://docs.google.com/document/d/1U2HzdXgm9SAJURptBRxkYbjiT6OzTz7Q0epU5Mw7WS0'
+    },
+    {
+        fullName: 'Md Moinur Mahbub Al Amin',
+        position: 'Web Developer',
+        title: 'MERN',
+        github: 'https://github.com/mdmmalamin',
+        linkedin: 'https://www.linkedin.com/in/mdmmalamin',
+        twitter: 'https://twitter.com/mdmmalamin',
+        website: ''
+    }
+];
